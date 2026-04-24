32ca75e3f20e1e9d0f0d437d5ebed672349cbdfe
thaismouramkt <thaismoura.ads@gmail.com>
2026-04-24 11:57:03 -0300

    Corrige aba para FAVORITOS, favicon e atualização otimista de favoritos

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,7 +2,7 @@ export default function Head() {
   return (
     <>
       <title>MediaScalers Offers</title>
-      <link rel="icon" href="/imagens/mediascalers_icon.svg" />
+      <link rel="icon" href="/mediascalers_icon.svg" />
       <meta name="description" content="MediaScalers Offers - painel de ofertas e tendências" />
       <meta property="og:title" content="MediaScalers Offers" />
       <meta property="og:description" content="MediaScalers Offers - painel de ofertas e tendências" />
